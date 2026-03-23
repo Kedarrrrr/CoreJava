df7ac683dc4ff6e41938e5fe51b621e64b7149a1
Kedarrrrr <132215546+Kedarrrrr@users.noreply.github.com>
2026-03-23 11:18:30 +0530

    Insertion Sort

---
+++ src/practice/InsertionSort.java
@@ -0,0 +1,30 @@
+package practice;
+
+public class InsertionSort {
+    
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];     // element to insert
+            int j = i - 1;
+
+            // shift elements to the right
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            // insert key at correct position
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 4, 1, 2};
+
+        insertionSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
