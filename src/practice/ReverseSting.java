4af5608c88f4b407fc3c7c1b4383285b3820cc2e
Kedarrrrr <132215546+Kedarrrrr@users.noreply.github.com>
2026-03-31 20:01:20 +0530

    Chore: DSA question

--- src/practice/ReverseSting.java
+++ src/practice/ReverseSting.java
@@ -4,6 +4,7 @@ public class ReverseSting {
     public static void main(String[] args) {
         String str = "hello";
         String reversed = new StringBuilder(str).reverse().toString();
+        //reverse
         System.out.println(reversed);
     }
 }
